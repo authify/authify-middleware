17bcdb665638ced7e93317825689c0c1799dcee4
Jonathan Gnagy <jgnagy@knuedge.com>
2017-02-06 10:26:39 -0800

    Blowing up less on failures

--- lib/authify/middleware/jwt_auth.rb
+++ lib/authify/middleware/jwt_auth.rb
@@ -3,45 +3,45 @@ module Authify
     # Auth Middleware
     class JWTAuth
       include Core::Helpers::JWTSSL
-
+      
       def initialize(app)
         @app = app
       end
 
       # rubocop:disable Metrics/MethodLength
       def call(env)
-        options = { algorithm: 'ES256', iss: CONFIG[:jwt][:issuer] }
-        bearer = env.fetch('HTTP_AUTHORIZATION', '').slice(7..-1)
-        payload, _header = JWT.decode bearer, public_key, true, options
+        options = {
+          algorithm: 'ES256',
+          verify_iss: true,
+          verify_iat: true,
+          iss: CONFIG[:jwt][:issuer]
+        }
+        begin
+          bearer = env.fetch('HTTP_AUTHORIZATION', '').slice(7..-1)
+          payload, _header = JWT.decode bearer, public_key, true, options
 
-        env[:scopes] = payload['scopes']
-        env[:user] = payload['user']
+          env[:scopes] = payload['scopes']
+          env[:user] = payload['user']
 
-        @app.call env
-      rescue JWT::DecodeError
-        [
-          401,
-          { 'Content-Type' => 'text/plain' },
-          ['A token must be passed.']
-        ]
-      rescue JWT::ExpiredSignature
-        [
-          403,
-          { 'Content-Type' => 'text/plain' },
-          ['The token has expired.']
-        ]
-      rescue JWT::InvalidIssuerError
-        [
-          403,
-          { 'Content-Type' => 'text/plain' },
-          ['The token does not have a valid issuer.']
-        ]
-      rescue JWT::InvalidIatError
-        [
-          403,
-          { 'Content-Type' => 'text/plain' },
-          ['The token does not have a valid "issued at" time.']
-        ]
+        rescue JWT::DecodeError => e
+          env[:authenticated] = false
+          env[:authentication_errors] ||= []
+          env[:authentication_errors] << e
+        rescue JWT::ExpiredSignature => e
+          env[:authenticated] = false
+          env[:authentication_errors] ||= []
+          env[:authentication_errors] << e
+        rescue JWT::InvalidIssuerError => e
+          env[:authenticated] = false
+          env[:authentication_errors] ||= []
+          env[:authentication_errors] << e
+        rescue JWT::InvalidIatError => e
+          env[:authenticated] = false
+          env[:authentication_errors] ||= []
+          env[:authentication_errors] << e
+        ensure
+          @app.call env
+        end
       end
     end
   end
