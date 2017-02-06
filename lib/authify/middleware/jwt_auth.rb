150d9346ceb72f416e43c6ca322a47d0480735c2
Jonathan Gnagy <jgnagy@knuedge.com>
2017-02-06 10:32:50 -0800

    Rubocop fixes

--- lib/authify/middleware/jwt_auth.rb
+++ lib/authify/middleware/jwt_auth.rb
@@ -10,38 +10,42 @@ module Authify
 
       # rubocop:disable Metrics/MethodLength
       def call(env)
+        payload = process_token
+
+        env[:scopes] = payload['scopes']
+        env[:user] = payload['user']
+      rescue JWT::DecodeError => e
+        env[:authenticated] = false
+        env[:authentication_errors] ||= []
+        env[:authentication_errors] << e
+      rescue JWT::ExpiredSignature => e
+        env[:authenticated] = false
+        env[:authentication_errors] ||= []
+        env[:authentication_errors] << e
+      rescue JWT::InvalidIssuerError => e
+        env[:authenticated] = false
+        env[:authentication_errors] ||= []
+        env[:authentication_errors] << e
+      rescue JWT::InvalidIatError => e
+        env[:authenticated] = false
+        env[:authentication_errors] ||= []
+        env[:authentication_errors] << e
+      ensure
+        @app.call env
+      end
+
+      private
+
+      def process_token
         options = {
           algorithm: 'ES256',
           verify_iss: true,
           verify_iat: true,
           iss: CONFIG[:jwt][:issuer]
         }
-        begin
-          bearer = env.fetch('HTTP_AUTHORIZATION', '').slice(7..-1)
-          payload, _header = JWT.decode bearer, public_key, true, options
 
-          env[:scopes] = payload['scopes']
-          env[:user] = payload['user']
-
-        rescue JWT::DecodeError => e
-          env[:authenticated] = false
-          env[:authentication_errors] ||= []
-          env[:authentication_errors] << e
-        rescue JWT::ExpiredSignature => e
-          env[:authenticated] = false
-          env[:authentication_errors] ||= []
-          env[:authentication_errors] << e
-        rescue JWT::InvalidIssuerError => e
-          env[:authenticated] = false
-          env[:authentication_errors] ||= []
-          env[:authentication_errors] << e
-        rescue JWT::InvalidIatError => e
-          env[:authenticated] = false
-          env[:authentication_errors] ||= []
-          env[:authentication_errors] << e
-        ensure
-          @app.call env
-        end
+        bearer = env.fetch('HTTP_AUTHORIZATION', '').slice(7..-1)
+        JWT.decode(bearer, public_key, true, options)[0]
       end
     end
   end
